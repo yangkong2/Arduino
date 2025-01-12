ed08f450e24a5476bacffe1569b6923b95c2879d
yangkong2 <ayeongyang64@gmail.com>
2025-01-12 15:51:37 +0900

    Add files via upload

---
+++ HallMagneitic/HallMagneitic.ino
@@ -0,0 +1,43 @@
+int DigitalPin = 7;  // Digital input
+
+int LedPin = 13;     // select the pin for the LED
+
+int gValue = 0;     // variable for Sensor Data
+
+void setup ()
+
+{
+
+  pinMode (LedPin, OUTPUT);
+
+  pinMode (DigitalPin, INPUT);
+
+  Serial.begin (9600);
+
+}
+
+void loop ()
+
+{
+
+  gValue = digitalRead(DigitalPin);
+
+  
+
+  if (gValue)   {        
+
+    digitalWrite (LedPin, HIGH);
+
+  }else {
+
+    digitalWrite (LedPin, LOW);
+
+  }
+
+  Serial.print ("Sensor Value : ");
+
+  Serial.println (gValue, DEC);
+
+  delay(500);
+
+}
