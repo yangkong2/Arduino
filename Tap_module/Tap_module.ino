d2436d083010b5fb6b0dcdf6d55bf760358ba21c
yangkong2 <ayeongyang64@gmail.com>
2025-02-27 19:27:38 +0900

    Add files via upload

---
+++ Tap_module/Tap_module.ino
@@ -0,0 +1,53 @@
+//*******************************************************************************
+
+// Project : 36_TAP_MODULE  in Sensor Kit
+
+// Board : Arduino Uno 
+
+// By : Kit Plus
+
+//*******************************************************************************
+
+//int AnalogPin = A0;    // Analog input
+
+int DigitalPin = 7;  // Digital input
+
+int LedPin = 13;     // select the pin for the LED
+
+int gValue = 0;     // variable for Sensor Data
+
+void setup ()
+
+{
+
+  pinMode (LedPin, OUTPUT);
+
+  pinMode (DigitalPin, INPUT);
+
+  Serial.begin (9600);
+
+}
+
+void loop ()
+
+{
+
+  gValue = digitalRead(DigitalPin);
+
+  
+
+  if (gValue){            
+
+    digitalWrite (LedPin, HIGH);
+
+  }else {
+
+    digitalWrite (LedPin, LOW);
+
+    Serial.println ("Detect TAP");
+
+     delay(1000);
+
+  }
+
+}
